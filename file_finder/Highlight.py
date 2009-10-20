5da102c8e48e7f67fd6fba197af7b21375313f12
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-10-20 20:56:45 +1100

    added highlight class

---
+++ file_finder/Highlight.py
@@ -0,0 +1,25 @@
+import re
+import logging
+
+class Highlight(object):
+	def __init__(self, query):
+		bits = query.replace('/',' ').split()
+		bits = sorted(bits, key=len, reverse=True)
+		bits = [bit.lower() for bit in bits]
+		searches = ["(%s)" % (re.escape(bit),) for bit in bits]
+		self.highlight_re = re.compile("(%s)" % ('|'.join(searches),))
+
+	def __call__(self, string):
+		last_end = 0
+		for match in self.highlight_re.finditer(string):
+			start, end = match.start(), match.end()
+			if start > last_end:
+				yield (False, string[last_end:start])
+			last_end = end
+			yield (True, string[start:end])
+		if last_end < len(string):
+			yield (False, string[last_end:])
+
+	def replace(self, string, replacement_func):
+		return self.highlight_re.sub(replacement_func('\\1'), string)
+
