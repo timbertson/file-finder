12902f57e84fcca012facc92cd077f4b4d6e2116
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-10-29 22:13:45 +1100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import *
 
 setup(
 	name='file-finder',
-	version='0.1.5',
+	version='0.1.6',
 	description='find and open files quickly',
 	author='Tim Cuthbertson',
 	author_email='tim3d.junk+findfiles@gmail.com',
