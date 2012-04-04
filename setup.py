00b01d75a6a3bb119d8f67375484235013195819
gfxmonk <tim@gfxmonk.net>
2012-04-04 22:49:00 +1000

    version 0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import *
 
 setup(
 	name='file-finder',
-	version='0.1.18',
+	version='0.2',
 	description='find and open files quickly',
 	author='Tim Cuthbertson',
 	author_email='tim3d.junk+findfiles@gmail.com',
